42a4afec45a5ecfef373e746291bdbe102a6af6f
aegoff <allysongoff@gmail.com>
2022-04-23 13:29:09 -0400

    added react-redux form to Directory page

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,114 @@
-import React from 'react'
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, Button, CardText, Modal, ModalHeader, ModalBody,CardBody, 
+    Breadcrumb, BreadcrumbItem, Col, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors} from 'react-redux-form';
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+class CommentForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            modal:false,
+            rating:'',
+            author:'',
+            text:'',
+            touched:{
+                rating: false,
+                author: false,
+                text: false
+            }
+        }
+        this.handleSubmit=this.handleSubmit.bind(this);
+        this.toggleModal=this.toggleModal.bind(this);
+    }
+    handleSubmit(values) {
+        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        
+    }
+    toggleModal(){
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+    render(){
+        return(
+            <>
+                <Button outline onClick={this.toggleModal}><i className="fa fa-pencil fa-lg"></i> Submit Comment</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Col md={10} offset={2}>
+                                <Label htmlFor="rating">Rating</Label>
+                                
+                                <Control.select model=".rating" id="rating" name="rating"
+                                        className="form-control"
+                                        >
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                </Control.select>
+                                </Col>                                
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={10} offset={2}>
+                                    <Label htmlFor="author" >Your Name</Label>
+                                        <Control.text model=".author" id="author" name="author"
+                                            placeholder="Your Name"
+                                            className="form-control"
+                                            validators={{
+                                                required,
+                                                minLength: minLength(2),
+                                                maxLength: maxLength(15)
+                                            }}
+                                        />
+                                        <Errors
+                                            className="text-danger"
+                                            model=".author"
+                                            show="touched"
+                                            component="div"
+                                            messages={{
+                                                required: 'Required',
+                                                minLength: 'Must be at least 2 characters',
+                                                maxLength: 'Must be 15 characters or less'
+                                            }}
+                                        />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={10} offset={2}>
+                                    <Label htmlFor="text">Comment</Label>
+                                    
+                                        <Control.textarea model=".text" id="text" name="text"
+                                            rows="5"
+                                            className="form-control"
+                                        />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={10}>
+                                    <Button type="submit" color="primary" >
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </>
+        );
+    }
+
+}
+
 
 function RenderComments({comments}){
         if (comments){
@@ -13,6 +121,7 @@ function RenderComments({comments}){
                     -{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </div>
                 )}
+                <CommentForm/>
             </div>);
         
 }
