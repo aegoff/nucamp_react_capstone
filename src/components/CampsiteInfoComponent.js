76e2e397fce98094641a52178eab55aa2c7e0512
aegoff <allysongoff@gmail.com>
2022-04-25 23:51:39 -0400

    redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, Button, CardText, Modal, ModalHeader, ModalBody,CardBody
     Breadcrumb, BreadcrumbItem, Col, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -145,7 +146,27 @@ function RenderCampsite({campsite}){
 }
 
 function CampsiteInfo(props) {
-        if (props.campsite) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    if (props.campsite) {
             return (
                 <div className="container">
                     <div className="row">
